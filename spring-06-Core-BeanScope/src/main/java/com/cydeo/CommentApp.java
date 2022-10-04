37972679332fb0f0bd69041cd82ea776fdbece11
nihano <nioztekin@gmail.com>
2022-10-04 17:03:19 +0300

    Bean Scope -singleton

---
+++ spring-06-Core-BeanScope/src/main/java/com/cydeo/CommentApp.java
@@ -0,0 +1,29 @@
+package com.cydeo;
+
+import com.cydeo.config.CommentConfig;
+import com.cydeo.model.Comment;
+import com.cydeo.service.CommentService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class CommentApp {
+    public static void main(String[] args) {
+
+        Comment comment = new Comment();
+        comment.setAuthor("Johnson");
+        comment.setText("Spring Framework");
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(CommentConfig.class);
+
+        CommentService cs1 = context.getBean(CommentService.class);
+        CommentService cs2 = context.getBean(CommentService.class);
+
+
+        //they are same objects
+        System.out.println(cs1); //com.cydeo.service.CommentService@130c12b7
+        System.out.println(cs2); //com.cydeo.service.CommentService@130c12b7
+        System.out.println(cs1==cs2);
+
+
+    }
+}
