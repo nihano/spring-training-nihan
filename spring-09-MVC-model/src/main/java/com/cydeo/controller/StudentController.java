487ef57dc93fdb4aebf75336559eb2c9b29c7583
nihano <nioztekin@gmail.com>
2022-10-07 01:38:53 +0300

    MVC Model

--- spring-09-MVC-model/src/main/java/com/cydeo/controller/StudentController.java
+++ spring-09-MVC-model/src/main/java/com/cydeo/controller/StudentController.java
@@ -4,6 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 @Controller
 public class StudentController{
 
@@ -11,6 +16,24 @@ public class StudentController{
     public String homePage(Model model) { //to execute a method from Model interface we passed the reference as parameter
         model.addAttribute("name", "Cydeo");
         model.addAttribute("course", "MVC");
+
+        String subject = "Spring Boot";
+        model.addAttribute("subject", subject);
+
+        int studentId = new Random().nextInt();
+        model.addAttribute("id", studentId);
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(6);
+        numbers.add(7);
+
+        model.addAttribute("numbers",numbers);
+
+        LocalDate dt = LocalDate.now();
+        model.addAttribute("date",dt);
+
 
         return "student/welcome"; //we are not putting .html for thymeleaf
     }
