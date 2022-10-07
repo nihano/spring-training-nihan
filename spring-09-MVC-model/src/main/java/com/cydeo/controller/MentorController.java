cfa123a594cd1d1d422bf47472fd7f23a75a84fd
nihano <nioztekin@gmail.com>
2022-10-07 12:55:33 +0300

    MVC Model

---
+++ spring-09-MVC-model/src/main/java/com/cydeo/controller/MentorController.java
@@ -0,0 +1,28 @@
+package com.cydeo.controller;
+
+
+import com.cydeo.enums.Gender;
+import com.cydeo.model.Mentor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class MentorController {
+
+    @RequestMapping("/list")
+    public String showTable(Model model) {
+
+        List<Mentor> mentorList = new ArrayList<>();
+        mentorList.add(new Mentor("Mike", "Smith", 45, Gender.MALE));
+        mentorList.add(new Mentor("Tom", "Hanks", 65, Gender.MALE));
+        mentorList.add(new Mentor("Ammy", "Bryan", 25, Gender.FEMALE));
+
+        model.addAttribute("mentors", mentorList);
+
+        return "mentor/mentor-list";
+    }
+}
