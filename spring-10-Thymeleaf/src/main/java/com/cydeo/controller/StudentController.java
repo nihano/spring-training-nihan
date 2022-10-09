48f25981233d10e7e395eb23194ae21663fda822
nihano <nioztekin@gmail.com>
2022-10-09 10:43:11 +0300

    Thymeleaf

--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -5,14 +5,15 @@ import com.cydeo.bootstrap.DataGenerator;
 import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("student")
 public class StudentController {
 
-    @RequestMapping("/register") //localhost:8080/student/register
+    //method = RequestMethod.GET is default we do not need to type. this is to retrieve the page.
+    //@RequestMapping(value = "/register", method = RequestMethod.GET) //localhost:8080/student/register
+    @GetMapping("/register") //this is same with @RequestMapping(value = "/register", method = RequestMethod.GET)
     public String register(Model model){
         model.addAttribute("students", DataGenerator.createStudent());
 
@@ -20,7 +21,9 @@ public class StudentController {
         return "student/register";
     }
 
-    @RequestMapping("/welcome") //localhost:8080/student/welcome
+    //Post==> when we search something etc. check class notes.
+    //@RequestMapping(value = "/welcome", method = RequestMethod.POST) //localhost:8080/student/welcome
+    @PostMapping("/welcome")
     public String welcome(@RequestParam String name){
 
         System.out.println(name);
