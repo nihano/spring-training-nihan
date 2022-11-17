fe117f504cc4b2b12c307bdbaa0801642339a3f5
nihano <nioztekin@gmail.com>
2022-11-17 14:42:17 -0500

    Spring Boot

--- spring-07-SpringBoot-Demo/src/main/java/com/cydeo/config/AppConfigData.java
+++ spring-07-SpringBoot-Demo/src/main/java/com/cydeo/config/AppConfigData.java
@@ -3,6 +3,7 @@ package com.cydeo.config;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 
 @Data
