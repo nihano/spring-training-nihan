b7d5973ff5f022b4003fc8f7e7dc01c0e44d8744
nihano <nioztekin@gmail.com>
2022-11-19 18:07:31 -0500

    Spring Data - Table Creation

--- spring-11-Data-TableCreation/src/main/resources/data.sql
+++ spring-11-Data-TableCreation/src/main/resources/data.sql
@@ -1,2 +1,10 @@
 INSERT INTO EMPLOYEES(id,name)
 VALUES (1, 'Mike');
+
+
+-- initial data that we want to put, one way is through SQL data.sql other way will learn later
+INSERT INTO students(first_name, last_name, email)
+VALUES ( 'Mike', 'Smith', 'mike@cydeo.com');
+
+INSERT INTO students(first_name, last_name, email)
+VALUES ( 'Tom', 'Hanks', 'tom@cydeo.com');
--- spring-11-Data-TableCreation/src/main/resources/schema.sql
+++ spring-11-Data-TableCreation/src/main/resources/schema.sql
@@ -1,3 +1,6 @@
+drop table if exists employees;
+
+
 create table EMPLOYEES(
                           id bigint not null,
                           name varchar(255),
