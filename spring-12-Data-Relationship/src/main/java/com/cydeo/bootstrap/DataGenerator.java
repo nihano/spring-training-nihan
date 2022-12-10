35c8728f08f59fbc807c5553654c142f43c2314c
nihano <nioztekin@gmail.com>
2022-12-10 13:49:51 +0300

    Spring Data Table Relationship

---
+++ spring-12-Data-Relationship/src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -0,0 +1,28 @@
+package com.cydeo.bootstrap;
+
+import com.cydeo.entity.Payment;
+import com.cydeo.entity.PaymentDetail;
+import com.cydeo.enums.Status;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Controller;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Controller
+public class DataGenerator implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+        Payment payment1 = new Payment(LocalDate.of(2022,4,19),new BigDecimal("150000"), Status.SUCCESS);
+        PaymentDetail paymentDetail1 = new PaymentDetail(new BigDecimal("140000"),new BigDecimal("10000"),LocalDate.of(2022,4,24));
+
+        payment1.setPaymentDetail(paymentDetail1);
+
+        Payment payment2 = new Payment(LocalDate.of(2022,4,25),new BigDecimal("100000"), Status.FAILURE);
+        PaymentDetail paymentDetail2 = new PaymentDetail(new BigDecimal("90000"),new BigDecimal("5000"),LocalDate.of(2022,4,29));
+
+        payment2.setPaymentDetail(paymentDetail2);
+
+    }
+}
