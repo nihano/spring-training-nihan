65dd426f2dc67577237f2d98cc4e7d8609f7a9bc
nihano <nioztekin@gmail.com>
2022-12-13 22:24:26 +0300

    Cinema Lab, table created

---
+++ spring-12-Data-Relationship/src/main/java/com/cydeo/entity/Customer.java
@@ -0,0 +1,35 @@
+package com.cydeo.entity;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@Data
+@Table(name = "customers")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String address;
+    private String email;
+    private String name;
+    private String surName;
+    private String userName;
+
+    @OneToMany (mappedBy = "customer")
+    private List<Payment> paymentList;
+
+    public Customer(String address, String email, String name, String surName, String userName) {
+        this.address = address;
+        this.email = email;
+        this.name = name;
+        this.surName = surName;
+        this.userName = userName;
+    }
+}
