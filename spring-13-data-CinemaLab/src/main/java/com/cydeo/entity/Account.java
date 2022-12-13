cc4658c8f37adb4388650de04567140d746646dd
nihano <nioztekin@gmail.com>
2022-12-13 22:23:59 +0300

    Cinema Lab, Table created

---
+++ spring-13-data-CinemaLab/src/main/java/com/cydeo/entity/Account.java
@@ -0,0 +1,28 @@
+package com.cydeo.entity;
+
+import com.cydeo.enums.UserRole;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ManyToAny;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "account_details")
+public class Account extends BaseEntity{
+
+    private String name;
+    private String address;
+    private String country;
+    private String state;
+    private String city;
+    private Integer age;
+    private String postalCode;
+    @Enumerated(EnumType.STRING)
+    private UserRole Role;
+    @OneToOne(mappedBy = "account")// we do not want 2 foreign key, there is no FK in AccountDetails table
+    private User user;
+
+}
