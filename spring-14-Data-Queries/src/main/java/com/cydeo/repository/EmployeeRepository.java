c4c8651796a534daccfa5ea9bb77b18e928a9cbb
nihano <nioztekin@gmail.com>
2022-12-16 01:14:39 +0300

    JPQL positional parameter

--- spring-14-Data-Queries/src/main/java/com/cydeo/repository/EmployeeRepository.java
+++ spring-14-Data-Queries/src/main/java/com/cydeo/repository/EmployeeRepository.java
@@ -1,6 +1,7 @@
 package com.cydeo.repository;
 
 import com.cydeo.entity.Employee;
+import org.hibernate.sql.Select;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -49,4 +50,43 @@ public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     @Query("SELECT e.salary FROM Employee e WHERE e.email='amcnee1@google.es'")
     Integer retrieveEmployeeSalary();
 
+    //Not Equal <> in SQL
+    @Query("SELECT e FROM Employee e WHERE e.salary <> ?1")
+    List<Employee> retrieveEmployeeSalaryNotEqual(int salary);
+
+    //Like / Contains / StartsWith /Endswith
+    //Like is a logical operator that is used to determine whether a specific character string matches a specified pattern.
+    @Query("SELECT e FROM Employee e WHERE e.firstName LIKE ?1")
+    List<Employee> retrieveEmployeeFirstNameLike(String pattern);
+
+    //Less than, Greater than
+    @Query("SELECT e FROM Employee e WHERE e.salary< ?1")
+    List<Employee> retrieveEmployeeSalaryLessThan(int salary);
+    @Query("SELECT e.firstName FROM Employee e WHERE e.salary> ?1")
+    List<String> retrieveEmployeeSalaryGreaterThan(int salary);
+
+    //Between
+    @Query("SELECT e FROM Employee e WHERE e.salary BETWEEN ?1 AND ?2")
+    List<Employee> retrieveEmployeeSalaryBetween(int salary1, int salary2);
+
+    //Before
+    @Query("SELECT e FROM Employee e WHERE e.hireDate> ?1")
+    List<Employee> retrieveEmployeeHireDateBefore (LocalDate date);
+
+    //NULL
+    @Query("SELECT e FROM Employee e WHERE e.email IS NULL")
+    List<Employee> retrieveEmployeeEmailIsNull();
+
+    //NOT NULL
+    @Query("SELECT e FROM Employee e WHERE e.email IS NOT NULL")
+    List<Employee> retrieveEmployeeEmailIsNotNull();
+
+    //SORTING in asc order
+    @Query("SELECT e FROM Employee e ORDER BY e.salary")
+    List<Employee> retrieveEmployeeSalaryOrderAsc();
+
+
+    //SORTING in desc order
+    @Query("SELECT e FROM Employee e ORDER BY e.salary DESC")
+    List<Employee> retrieveEmployeeSalaryOrderDesc();
 }
