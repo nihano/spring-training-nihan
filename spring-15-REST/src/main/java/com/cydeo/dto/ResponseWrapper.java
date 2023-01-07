422fc806674225710d90e1631098e04f4361227b
nihano <nioztekin@gmail.com>
2023-01-07 04:12:10 +0300

    ResponseWrapper

--- spring-15-REST/src/main/java/com/cydeo/dto/ResponseWrapper.java
+++ spring-15-REST/src/main/java/com/cydeo/dto/ResponseWrapper.java
@@ -10,18 +10,20 @@ import org.springframework.http.HttpStatus;
 @NoArgsConstructor
 public class ResponseWrapper {
 
+    //to make JSON custom we added some fields and Object for dto
     private boolean success;
     private String message;
     private Integer code;
-    private Object data;
+    private Object data; //DTOs
 
-    public ResponseWrapper(String message,Object data){
-        this.message = message;
-        this.data=data;
+    public ResponseWrapper(String message, Object data){
+        this.message =message;
+        this.data = data;
         this.code= HttpStatus.OK.value();
         this.success=true;
     }
 
+    //ex: for delete we will not show data
     public ResponseWrapper(String message){
         this.message=message;
         this.code=HttpStatus.OK.value();
