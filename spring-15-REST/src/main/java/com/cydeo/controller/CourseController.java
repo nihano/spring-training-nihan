5c8f796d1bbcf9a393fd6e2d00b113a69f9ef5ac
nihano <nioztekin@gmail.com>
2023-01-07 04:11:07 +0300

    Course controller - creating APIs CRUD

--- spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
+++ spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
@@ -2,12 +2,11 @@ package com.cydeo.controller;
 
 import com.cydeo.dto.CourseDTO;
 import com.cydeo.service.CourseService;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController //@Controller + @ResponseBody
+@RestController //(@Controller + @ResponseBody)
 @RequestMapping("/courses/api/v1")
 public class CourseController {
 
@@ -18,39 +17,44 @@ public class CourseController {
     }
 
     @GetMapping
+//    @ResponseBody we do not need @RestController (@Controller + @ResponseBody)
     public List<CourseDTO> getAllCourses(){
-
-        return courseService.getCourses();
+        return courseService.getCourses();//we are not returning view anymore
     }
 
-    @GetMapping("{id}")
+    @GetMapping({"{id}"})
     public CourseDTO getCourseById(@PathVariable("id") long courseId){
         return courseService.getCourseById(courseId);
     }
 
-    @GetMapping("category/{name}")
+    @GetMapping("category/{name}") //localhost:8080/courses/category/Spring in postman with GET and send request to get JSON
     public List<CourseDTO> getCourseByCategory(@PathVariable("name") String category){
         return courseService.getCoursesByCategory(category);
-
     }
 
     @PostMapping
-    public CourseDTO createCourse(@RequestBody CourseDTO course){
-
+    public CourseDTO createCourse(@RequestBody CourseDTO course){ //to capture the JSON body
         return courseService.createCourse(course);
     }
+    /*
+     JSON body POST Example = we used @JasonIgnore to not display id=null in response
+    {
+    "name": "Spring REST",
+    "category": "Spring",
+    "rating": 4,
+    "description": "Spring Boot gives all the power of the Spring Framework without all of the complexities"
+    }
+     */
 
     @PutMapping("{id}")
-    public void updateCourse(@PathVariable("id") long courseId,@RequestBody CourseDTO course){
-        courseService.updateCourse(courseId,course);
+    public void updateCourse(@PathVariable("id") long courseId, @RequestBody CourseDTO course){
+        courseService.updateCourse(courseId, course);
     }
 
     @DeleteMapping("{id}")
     public void deleteCourseById(@PathVariable("id") long courseId){
         courseService.deleteCourseById(courseId);
-
     }
-
 
 
 
