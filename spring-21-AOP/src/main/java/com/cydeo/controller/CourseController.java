ce277949a7bd80ca2059c90fbf66da34b8398ba0
nihano <nioztekin@gmail.com>
2023-01-14 01:51:50 +0300

    adds LoggingAnnotation

--- spring-21-AOP/src/main/java/com/cydeo/controller/CourseController.java
+++ spring-21-AOP/src/main/java/com/cydeo/controller/CourseController.java
@@ -1,5 +1,6 @@
 package com.cydeo.controller;
 
+import com.cydeo.annotation.LoggingAnnotation;
 import com.cydeo.dto.CourseDTO;
 import com.cydeo.service.CourseService;
 import org.springframework.web.bind.annotation.*;
@@ -24,6 +25,7 @@ public class CourseController {
         return list;
     }
 
+    @LoggingAnnotation
     @GetMapping("/{id}")
     public CourseDTO getCourseById(@PathVariable("id") Long courseId) {
         return courseService.getCourseById(courseId);
@@ -33,7 +35,7 @@ public class CourseController {
     public List<CourseDTO> getCourseByCategory(@PathVariable("name") String category) {
         return courseService.getCoursesByCategory(category);
     }
-
+    @LoggingAnnotation
     @PostMapping
     public CourseDTO createCourse(@RequestBody CourseDTO course) {
         return courseService.createCourse(course);
