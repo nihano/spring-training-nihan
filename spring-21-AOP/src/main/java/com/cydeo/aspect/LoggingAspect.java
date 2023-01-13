06b948b7c8ffee7c177b2edbff9d5e0878f22bbf
nihano <nioztekin@gmail.com>
2023-01-14 01:52:41 +0300

    LoggingAspect - custom annotation, AfterReturning, AfterThrowing

--- spring-21-AOP/src/main/java/com/cydeo/aspect/LoggingAspect.java
+++ spring-21-AOP/src/main/java/com/cydeo/aspect/LoggingAspect.java
@@ -1,12 +1,14 @@
 package com.cydeo.aspect;
 
+import com.cydeo.dto.CourseDTO;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Aspect
 @Component
@@ -28,16 +30,72 @@ public class LoggingAspect {
 //    public void log(){
 //        logger.info("Info log.........");
 //    }
+//
+//    @Pointcut("execution(* com.cydeo.repository.CourseRepository.findById(*))")
+//    public void courseRepositoryFindByIdPc() {
+//    }
+//
+//    @Before("courseRepositoryFindByIdPc()")
+//    public void beforeCourseRepositoryFindById(JoinPoint joinPoint) {
+//        logger.info("Before -> Method: {}, Arguments: {}, Target: {}"
+//                , joinPoint.getSignature(), joinPoint.getArgs(), joinPoint.getTarget());
+//    }
+//
+//
+//    @Pointcut("within(com.cydeo.controller..*)")
+//    //any method/anything inside controller package and if controller package has any subpackages
+//    public void anyControllerOperation() {}
+//
+//    @Pointcut("@within(org.springframework.stereotype.Service)")
+//    public void anyServiceOperation() {}
+//
+//    @Before("anyControllerOperation() || anyServiceOperation()")
+//    public void beforeControllerOrServiceAdvice(JoinPoint joinPoint) {
+//        logger.info("Before -> Method: {}, Arguments: {}, Target: {}"
+//                , joinPoint.getSignature(), joinPoint.getArgs(), joinPoint.getTarget());
+//    }
+//
+//    @Pointcut("@annotation(org.springframework.web.bind.annotation.DeleteMapping)")
+//    public void anyDeleteControllerOperation(){}
+//
+//    @Before("anyDeleteControllerOperation()")
+//    public void beforeDeleteMappingAnnotation(JoinPoint joinPoint) {
+//        logger.info("Before -> Method: {}, Arguments: {}, Target: {}"
+//                , joinPoint.getSignature(), joinPoint.getArgs(), joinPoint.getTarget());
+//  }
+//    @Pointcut("@annotation(com.cydeo.annotation.LoggingAnnotation)")
+//    public void loggingAnnotationPC(){}
+//
+//    @Before("loggingAnnotationPC()")
+//    public void beforeLoggingAnnotation(JoinPoint joinPoint) {
+//        logger.info("Before -> Method: {}, Arguments: {}, Target: {}"
+//                , joinPoint.getSignature(), joinPoint.getArgs(), joinPoint.getTarget());
+//    }
 
-    @Pointcut("execution(* com.cydeo.repository.CourseRepository.findById(*))")
-    public void courseRepositoryFindByIdPc() {
-    }
+//    @Pointcut("@annotation(org.springframework.web.bind.annotation.GetMapping)")
+//    public void afterReturningGetMappingAnnotation(){}
 
-    @Before("courseRepositoryFindByIdPc()")
-    public void beforeCourseRepositoryFindById(JoinPoint joinPoint) {
-        logger.info("Before -> Method: {}, Arguments: {}, Target: {}"
-                , joinPoint.getSignature(), joinPoint.getArgs(), joinPoint.getTarget());
-    }
+//    @AfterReturning(pointcut = "afterReturningGetMappingAnnotation()", returning = "result")
+//    public void afterReturningGetMappingOperation(JoinPoint joinPoint, Object result){
+//        logger.info("After Returning -> Method: {}, Result: {}", joinPoint.getSignature(), result.toString());
+//    }
+
+    //CourseDTO -> Object this is ok
+    //List<CourseDTO>-> List<Object> this is not ok because of generics
+//    @AfterReturning(pointcut = "afterReturningGetMappingAnnotation()", returning = "results")
+//    public void afterReturningGetMappingOperation(JoinPoint joinPoint, List<CourseDTO> results){
+//        logger.info("After Returning -> Method: {}, Results: {}", joinPoint.getSignature(), results.toString());
+//    }
+//
+//    @AfterThrowing(pointcut = "afterReturningGetMappingAnnotation()", throwing = "exception")
+//    public void afterThrowingException(JoinPoint joinPoint, RuntimeException exception){
+//        logger.error("After Throwing -> Method: {}, Exception: {}"
+//        , joinPoint.getSignature().toShortString(), exception.getMessage());
+//    }
+
+
+
+
 
 
 }
