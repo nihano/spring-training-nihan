3ada4b8277ec4202eda5398a4c647e9551529d2b
nihano <nioztekin@gmail.com>
2023-01-15 01:28:46 +0300

    spring-22-UnitTesting-JUnit5 - assertions and before after methods added

---
+++ spring-22-UnitTesting-JUnit5/src/test/java/CalculatorTest.java
@@ -0,0 +1,95 @@
+import org.junit.jupiter.api.*;
+
+import java.nio.file.AccessDeniedException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @BeforeAll
+    static void setUpAll(){
+        System.out.println("BeforeAll is executed.");
+
+    }
+
+    @AfterAll
+    static void tearDownAll(){
+        System.out.println("AfterAll is executed.");
+    }
+
+    @BeforeEach
+    void setUpEach(){
+        System.out.println("BeforeEach is executed.");
+
+    }
+
+    @AfterEach
+    void tearDownEach(){
+        System.out.println("AfterEach is executed.");
+
+    }
+
+    @Test
+    void add() {
+        System.out.println("Add method");
+        int actual = Calculator.add(2, 3);
+        assertEquals(5, actual, "Test failed.");
+    }
+
+    @Test
+    void add2() {
+        System.out.println("Add2 method");
+
+//        assertThrows(IllegalArgumentException.class, () -> Calculator.add2(3,2)); //pass
+//        assertThrows(AccessDeniedException.class, () -> Calculator.add2(3,2)); //fail
+        assertThrows(IllegalArgumentException.class, (() -> Calculator.add2(2, 3))); //no exception thrown fail
+    }
+
+    @Test
+    void testCase1() {
+//        System.out.println("Test Case 1");
+        fail("Not implemented yet");
+    }
+
+    @Test
+    void testCase2() {
+        System.out.println("Test Case 2");
+        assertEquals("add", Calculator.operator);
+        assertTrue(Calculator.operator.equals("add"));
+    }
+
+    @Test
+    void testCase3() {
+        System.out.println("Test Case 3");
+        assertArrayEquals(new int[]{1, 2, 3}, new int[]{1, 3, 2}, "Arrays are not same.");
+    }
+
+    @Test
+    void testCase4() {
+        System.out.println("Test Case 4");
+
+        String nullString = null;
+        String notNullString = "Cydeo";
+
+        assertNull(nullString);
+        assertNotNull(notNullString);
+
+        assertNull(notNullString);
+        assertNotNull(nullString);//if above fails this will not get executed
+
+    }
+
+    @Test
+    void testCase5() {
+        System.out.println("Test Case 5");
+
+        Calculator c1 = new Calculator();
+        Calculator c2 = c1;
+        Calculator c3 = new Calculator();
+
+        assertSame(c1, c2);
+        assertNotSame(c1, c3);
+    }
+
+
+}
